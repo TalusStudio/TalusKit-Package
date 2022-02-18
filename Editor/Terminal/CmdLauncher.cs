3f1448702e93cb088a6306206e7500b7f5dbbb48
Emre - Win <kovanciemre@gmail.com>
2022-02-18 22:39:22 +0300

    Add terminal utility

---
+++ Editor/Terminal/CmdLauncher.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace KRT.UnityTerminalLauncher
+{
+    class CmdLauncher : TerminalLauncher
+    {
+        internal override bool HasExecutable => ExistsOnPath("cmd.exe");
+
+        internal override Process Launch(string targetFolder)
+        {
+            var processInfo = new ProcessStartInfo("cmd.exe")
+            {
+                WorkingDirectory = targetFolder,
+            };
+            return Process.Start(processInfo);
+        }
+    }
+}
