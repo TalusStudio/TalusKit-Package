b4553a91282f1c07e1f3d004cef662ed195061f2
Emre Kovanci <kovanciemre@gmail.com>
2022-06-15 01:58:52 +0300

    add asset organizer

---
+++ Editor/AssetOrganizer/AssetOrganizerWatchedFolder.cs
@@ -0,0 +1,39 @@
+using UnityEditor;
+using UnityEngine;
+
+using Sirenix.OdinInspector;
+
+namespace TalusKit.Editor.AssetOrganizer
+{
+    [CreateAssetMenu(menuName = "_OTHERS/Asset Organizer/Watched Folder")]
+    public class AssetOrganizerWatchedFolder : ScriptableObject
+    {
+        [FolderPath(UseBackslashes = true)]
+        [ValueColor(nameof(PathStatusColor))]
+        [OnValueChanged(nameof(DisableSubdirectoriesIfNecessary))]
+        public string Path;
+
+        [DisableIf("@Path == \"Assets\"")]
+        [TableColumnWidth(150, Resizable = false)]
+        [ValueDropdown(nameof(subdirectoryOptions))]
+        public bool IncludeSubdirectories;
+
+        private readonly ValueDropdownList<bool> subdirectoryOptions = new ValueDropdownList<bool>
+        {
+            { "Yes", true },
+            { "No", false }
+        };
+
+        private Color PathStatusColor => AssetDatabase.IsValidFolder(Path)
+            ? AssetOrganizerColors.Success
+            : AssetOrganizerColors.Error;
+
+        private void DisableSubdirectoriesIfNecessary()
+        {
+            if (Path == "Assets")
+            {
+                IncludeSubdirectories = false;
+            }
+        }
+    }
+}
---
+++ Editor/AssetOrganizer/ValueColorAttribute/ValueColorAttribute.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Diagnostics;
+
+using UnityEngine;
+
+namespace TalusKit.Editor.AssetOrganizer
+{
+    [Conditional("UNITY_EDITOR")]
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = true)]
+    public class ValueColorAttribute : Attribute
+    {
+        public Color Color;
+        public string GetColor;
+
+        public ValueColorAttribute(string getColor) => GetColor = getColor;
+        public ValueColorAttribute(float r, float g, float b, float a = 1f) => Color = new Color(r, g, b, a);
+    }
+}
